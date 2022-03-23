d414ec1e1046a5a1715b1a67d8244d0657bbd10e
Mohamed <mohamedfawz2010@gmail.com>
2022-03-23 13:44:25 +0200

    fawzy

---
+++ app/src/main/java/com/apps/wound_fairy/model/SendOrderModel.java
@@ -0,0 +1,9 @@
+package com.apps.wound_fairy.model;
+
+import androidx.databinding.BaseObservable;
+
+public class SendOrderModel extends BaseObservable {
+//    private int product_id;
+//    private int amount;
+//    private String latitude
+}
